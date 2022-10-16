fe2a53284399e23797d72d5752cd2c2b5d3bd5d5
stevancodes <stevan.code@gmail.com>
2022-10-16 11:23:45 +0200

    Active classes upgraded

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,30 +1,40 @@
-import { Link } from 'react-router-dom';
-import { useState } from 'react'
-
+import { Link, useLocation } from "react-router-dom";
+import { useState } from "react";
 
 const Navbar = () => {
-    const [notActive, setActive] = useState(false)
-    const [notActive2, setActive2] = useState(false)
+  const [notActive, setActive] = useState(false);
+  const [notActive2, setActive2] = useState(false);
+  const location = useLocation();
 
-    const toggleActive = () => {
-        setActive2(false)
-        const currentState = notActive
-        setActive(!currentState)
-    }
-    const toggleActive2 = () => {
-        setActive(false)
-        const currentState2 = notActive2
-        setActive2(!currentState2)
-    }
-    return (
-        <nav className="navbar">
-            <h1>BitBlog</h1>
-            <div className="links">
-                <Link to="/" className={notActive ? "active" : null} onClick={toggleActive}>Home</Link>
-                <Link to="/create" className={notActive2 ? "active" : null} onClick={toggleActive2}>New Blog</Link>
-            </div>
-        </nav >
-    );
-}
+  const toggleActive = () => {
+    setActive2(false);
+    const currentState = notActive;
+    setActive(!currentState);
+  };
+  const toggleActive2 = () => {
+    setActive(false);
+    const currentState2 = notActive2;
+    setActive2(!currentState2);
+  };
+
+  const className = (path) => {
+    if (location.pathname === path) return "active";
+    return null;
+  };
+
+  return (
+    <nav className="navbar">
+      <h1>BitBlog</h1>
+      <div className="links">
+        <Link to="/" className={className("/")} /*onClick={toggleActive}*/>
+          Home
+        </Link>
+        <Link to="/create" className={className("/create")} /*onClick={toggleActive2}*/>
+          New Blog
+        </Link>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
